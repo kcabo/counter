5c809c1a7cf0b4518175d2b5fb58226ce1f7e5d2
Kcabo <37576764+kcabo@users.noreply.github.com>
2021-06-05 00:17:10 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -19,17 +19,36 @@ const paan = document.querySelector<HTMLAudioElement>("#paan");
 
 const buttons = document.querySelectorAll<HTMLElement>('.swiper-slide');
 buttons.forEach((button) => {
-    button.addEventListener('click', (event) => {
-        const valstr = button.innerHTML;
-        const val  = parseInt(valstr) + 1;
-        button.innerHTML = String(val);
-        if (val % 10 == 0){
-            paan.load();
-            paan.play();
-        } else {
-            click.load();
-            click.play();
-        }
-    })
+    button.addEventListener('click', countUp);
 });
 
+function countUp(event) {
+    const val = parseInt(event.currentTarget.innerHTML) + 1;
+    event.currentTarget.innerHTML = String(val);
+    if (val % 10 == 0){
+        paan.load();
+        paan.play();
+    } else {
+        click.load();
+        click.play();
+    }
+};
+
+const logDiv = document.querySelector<HTMLElement>('#log');
+
+
+swiper.on('slideNextTransitionEnd', () => {
+    const buttons = document.querySelectorAll<HTMLElement>('.swiper-slide');
+    const finishButton = buttons[swiper.activeIndex - 1];
+    const count = parseInt(finishButton.innerHTML);
+    const valLog = String(count) + ', ';
+    logDiv.innerHTML = logDiv.innerHTML + valLog;
+});
+
+swiper.on('reachEnd', () => {
+    let newSlide = document.createElement('div');
+    newSlide.classList.add('swiper-slide');
+    newSlide.innerHTML = '0';
+    newSlide.addEventListener('click', countUp);
+    swiper.appendSlide(newSlide);
+});
