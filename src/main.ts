cc6c386ab51321c53e0ad4d58352fa8b756bfa29
Kcabo <37576764+kcabo@users.noreply.github.com>
2021-06-04 23:02:25 +0900

    add audio

--- src/main.ts
+++ src/main.ts
@@ -13,11 +13,18 @@ const option = {
 
 const swiper = new Swiper('.mySwiper', option);
 
+const bgm1 = document.querySelector("audio");  
+
+
 const buttons = document.querySelectorAll<HTMLElement>('.swiper-slide');
 buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
         const valstr = button.innerHTML;
         const val  = parseInt(valstr) + 1;
         button.innerHTML = String(val);
+        if (val % 5 == 0){
+            bgm1.play();
+        }
     })
 });
+
